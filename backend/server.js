6c21e6332707ad2c35fe936dae04ad84ef0b7c6d
Asure <107452230+kevin-dangol@users.noreply.github.com>
2025-11-04 22:35:50 +0545

    tables

--- backend/server.js
+++ backend/server.js
@@ -21,3 +21,29 @@ db.getConnection()
 app.listen(3001, () => {
     console.log('Server running on port 3001');
 });
+
+
+// creating tables
+
+(async () => {
+  try {
+    const conn = await db.getConnection();
+
+    await conn.query(`
+      CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            username VARCHAR(50) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            is_admin BOOLEAN NULL,
+            c_id INT NULL,
+            s_id INT NULL
+        );
+    `);
+
+    conn.release();
+    console.log('✅ Users table created or already exists');
+  } catch (err) {
+    console.error('❌ Failed to create table:', err);
+  }
+})();
