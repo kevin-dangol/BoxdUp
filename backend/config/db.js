588672e1d30152c709ae1909e8a1abbbef311a06
Asure <107452230+kevin-dangol@users.noreply.github.com>
2025-11-04 22:09:00 +0545

    db transfered to railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database:'boxdup'
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Connected to MySQL');
+  }
 });
 
 module.exports = db;
