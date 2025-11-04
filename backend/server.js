c6b84c81d89350dcdbb2bc572dc432c59adc33c1
Asure <107452230+kevin-dangol@users.noreply.github.com>
2025-11-04 23:13:57 +0545

    db origin 2

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use('/api/auth', authRoutes);
 app.use(express.static(path.join(__dirname, '../')));
 
 app.use((req, res, next) => {
+  if (req.path.startsWith('/api/')) return next();
   res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
